016e4a1aba2e3b25f416270e2e189355b18b1a61
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2026-01-30 10:50:17 -0300

    Torna Nome obrigatório na classe Biblioteca
    A propriedade Nome agora utiliza o modificador required,
    garantindo que um valor seja informado ao instanciar a classe
    Biblioteca. Isso reforça a integridade dos dados do modelo.

--- Models/Biblioteca.cs
+++ Models/Biblioteca.cs
@@ -3,7 +3,7 @@
     public class Biblioteca
     {
         public int BibliotecaId { get; set; }
-        public string Nome { get; set; }
+        public required string Nome { get; set; }
         public string Endereco { get; set; }
         public string Telefone { get; set; }
         public int Quantidade { get; set; }
