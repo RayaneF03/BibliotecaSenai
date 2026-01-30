e89a37fe5ad9fe63b3a8fd4d4db53186ea204bfe
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2026-01-30 09:10:15 -0300

    Adiciona classe Livro com propriedades básicas
    Criado o arquivo Livro.cs contendo a classe Livro no namespace BibliotecaSenai.Models, incluindo propriedades para LivroId, Titulo, Autor, Genero, AnoPublicacao e Editora, representando os dados essenciais de um livro no sistema.

---
+++ Models/Livro.cs
@@ -0,0 +1,12 @@
+﻿namespace BibliotecaSenai.Models
+{
+    public class Livro
+    {
+        public int LivroId { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string Genero { get; set; }
+        public int AnoPublicacao { get; set; }
+        public string Editora { get; set; }
+    }
+}
